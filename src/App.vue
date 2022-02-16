c957048e3e94e4b92407190d6b8289febb15477a
stevecyj <ycsteven.tsao@gmail.com>
2022-02-16 23:43:29 +0800

    prettierrc,vue.config,deploy.sh

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    JOJO
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
